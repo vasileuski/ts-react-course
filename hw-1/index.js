eaad0495cdb1b74f2e1aaee9af6e6250bb988392
Artsiom Vasileuski <vasileuski.artsiom@gmail.com>
2022-07-25 02:13:00 +0400

    add: complete hw

--- hw-1/index.js
+++ hw-1/index.js
@@ -1,5 +1,8 @@
+let numbersArr = [3, 5, 2, 1, 6, 8, 9];
+
+// Array.map
 Array.prototype.customMap = function (callback) {
-  const result = [];
+  let result = [];
   for (let i = 0; i < this.length; i++) {
     result.push(callback(this[i], i, this));
   }
@@ -7,8 +10,7 @@ Array.prototype.customMap = function (callback) {
   return result;
 };
 
-let numbersArr = [3, 5, 2, 1, 6, 8, 9];
-
+// Array.reduce
 Array.prototype.customReduce = function (callback, initValue) {
   let acc = arguments.length >= 2 ? initValue : this[0];
   let iStart = arguments.length >= 2 ? 0 : 1;
@@ -19,6 +21,24 @@ Array.prototype.customReduce = function (callback, initValue) {
   return acc;
 };
 
-let newArr = numbersArr.customReduce((value) => value + 10);
+let reduceExample = numbersArr.customReduce((value) => value + 10);
 
-console.log(newArr);
+// Array.filter
+Array.prototype.customFilter = function (callback) {
+  let result = [];
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      result.push(this[i]);
+    }
+  }
+  return result;
+};
+
+let filterExample = numbersArr.customFilter((el) => el > 5);
+
+// Array.forEach
+Array.prototype.customForEach = function (callback) {
+  for (let i = 0; i < this.length; i++) {
+    callback(this[i], i, this);
+  }
+};
