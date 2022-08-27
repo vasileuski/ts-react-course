2d1351d1da8f6002e631c53ed9b8460d7b69e1dd
Artsiom Vasileuski <vasileuski.artsiom@gmail.com>
2022-08-27 21:34:28 +0400

    fix: hw-3

--- hw-3/index.ts
+++ hw-3/index.ts
@@ -1,43 +1,42 @@
 interface Users {
-    name: string,
-    phone: string,
-    email: string,
-    animals?: string[],
-    cars?: string[],
-    hasChildren: boolean,
-    hasEducation: boolean
+  name: string;
+  phone: string;
+  email: string;
+  animals?: string[];
+  cars?: string[];
+  hasChildren: boolean;
+  hasEducation: boolean;
 }
 
 const users: Users[] = [
-    {
-        name: "Harry Felton",
-        phone: "(09) 897 33 33",
-        email: "felton@gmail.com",
-        animals: ["cat"],
-        cars: ["bmw"],
-        hasChildren: false,
-        hasEducation: true
-        
-    },
-    {
-        name: "May Sender",
-        phone: "(09) 117 33 33",
-        email: "sender22@gmail.com",
-        hasChildren: true,
-        hasEducation: true
-    },
-    {
-        name: "Henry Ford",
-        phone: "(09) 999 93 23",
-        email: "ford0@gmail.com",
-        cars: ["bmw", "audi"],
-        hasChildren: true,
-        hasEducation: false
-    }
-]
+  {
+    name: "Harry Felton",
+    phone: "(09) 897 33 33",
+    email: "felton@gmail.com",
+    animals: ["cat"],
+    cars: ["bmw"],
+    hasChildren: false,
+    hasEducation: true,
+  },
+  {
+    name: "May Sender",
+    phone: "(09) 117 33 33",
+    email: "sender22@gmail.com",
+    hasChildren: true,
+    hasEducation: true,
+  },
+  {
+    name: "Henry Ford",
+    phone: "(09) 999 93 23",
+    email: "ford0@gmail.com",
+    cars: ["bmw", "audi"],
+    hasChildren: true,
+    hasEducation: false,
+  },
+];
 
 // 1. Создать строку из имен пользователей через запятую
-const getUserNames: string = users.map((user) => user.name).join(', ');
+const getUserNames: string = users.map((user) => user.name).join(", ");
 
 // 2. Посчитать общее количнство машин у пользователей
 const carsTotal: number = users.reduce((acc, user) => {
@@ -62,4 +61,10 @@ function sortAnimals(users: Users[]): Users[] {
 }
 
 // 5. Создать функцию, которая бы принимала массив пользователей и отдавала бы  строку с названиями марок автомобилей через запятую
-const getCarNames: string = users.map((user) => user.cars).join(","); // я не придумал как красиво это вывести(((
+// const getCarNames: string = users.map((user) => user.cars).join(",");
+
+function getCarNames2(arr): string {
+  return arr.map((user) => user.cars).join(",");
+}
+
+console.log(getCarNames2(users));
